4a2c73e54f26b2c0fb8316a5598e22638e760adc
otamm <oivatom@gmail.com>
2015-07-20 04:52:38 -0400

    Added physics; made ground1 and ground2 static bodies

--- Source/MainScene.swift
+++ Source/MainScene.swift
@@ -5,4 +5,16 @@ class MainScene: CCNode {
     
     // the bird sprite
     weak var bird:CCSprite!;
+    
+    // first ground; first and second grounds will be rendered one after another to give the impression of movement.
+    weak var ground1:CCSprite!;
+    
+    // second ground block.
+    weak var ground2:CCSprite!;
+    
+    // the main physics node, every child of it are affected by physics.
+    weak var gamePhysicsNode:CCPhysicsNode!;
+    
+    /* custom variables */
+    
 }
