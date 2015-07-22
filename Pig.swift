36d4dfb5b612a7f34551bc9ea9be308bf413f293
otamm <oivatom@gmail.com>
2015-07-21 23:55:49 -0400

    Added pig class, pig as a physics sensor body and dying animation methods

---
+++ Pig.swift
@@ -0,0 +1,20 @@
+//
+//  Pig.swift
+//  AngryTimberyBird
+//
+//  Created by Otavio Monteagudo on 7/21/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import Foundation;
+
+class Pig:CCSprite {
+    
+    func didLoadFromCCB() {
+        self.physicsBody.sensor = true;
+    }
+    
+    func die() {
+        animationManager.runAnimationsForSequenceNamed("Die");
+    }
+}
